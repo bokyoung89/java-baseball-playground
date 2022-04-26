2c5df609ba7f408a400670d23448789fe48f5c4d
sbk8689 <sbk8689@gmail.com>
2022-04-26 23:00:09 +0900

    feat: 사칙연산 구현 및 출력 확인

---
+++ src/main/java/Calculate.java
@@ -0,0 +1,18 @@
+public class Calculate {
+
+    int add(int i, int j){
+        return i + j;
+    }
+
+    int minus(int i, int j){
+        return i - j;
+    }
+
+    int multiply(int i, int j){
+        return i * j;
+    }
+
+    int divide(int i, int j){
+        return i / j;
+    }
+}
