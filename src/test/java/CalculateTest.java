169da40b4de4077dcd2d6fa70934272490cd7c22
sbk8689 <sbk8689@gmail.com>
2022-04-26 23:01:19 +0900

    test: 사칙연산 구현 및 출력 테스트

---
+++ src/test/java/CalculateTest.java
@@ -0,0 +1,33 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CalculateTest {
+
+    Calculate cal = new Calculate();
+
+    @Test
+    @DisplayName("사칙연산 구현 테스트_덧셈")
+    void addTest(){
+        assertThat(cal.add(3,4)).isEqualTo(7);
+    }
+
+    @Test
+    @DisplayName("사칙연산 구현 테스트_뺄셈")
+    void minusTest(){
+        assertThat(cal.minus(5,2)).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("사칙연산 구현 테스트_곱셈")
+    void multiplyTest(){
+        assertThat(cal.multiply(4,5)).isEqualTo(20);
+    }
+
+    @Test
+    @DisplayName("사칙연산 구현 테스트_나눗셈")
+    void divideTest(){
+        assertThat(cal.divide(6,2)).isEqualTo(3);
+    }
+}
