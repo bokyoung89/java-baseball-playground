9a5b150211e9d2891623ae0e960d26404c14b68e
sbk8689 <sbk8689@gmail.com>
2022-06-27 19:47:41 +0900

    feat : 문자열을 포함한 사칙연산 구현

--- src/main/java/Calculate.java
+++ src/main/java/Calculate.java
@@ -1,18 +1,34 @@
 public class Calculate {
 
-    int add(int i, int j){
+    int add(int i, int j) {
         return i + j;
     }
 
-    int minus(int i, int j){
+    int minus(int i, int j) {
         return i - j;
     }
 
-    int multiply(int i, int j){
+    int multiply(int i, int j) {
         return i * j;
     }
 
-    int divide(int i, int j){
+    int divide(int i, int j) {
         return i / j;
+    }
+
+    static int calculate(int first, String symbol, int second) {
+        int result = 0;
+        if (symbol.equals("+")) {
+            result = first + second;
+        } else if (symbol.equals("-")) {
+            result = first - second;
+        } else if (symbol.equals("*")) {
+            result = first * second;
+        } else if (symbol.equals("/")) {
+            result = first / second;
+        } else {
+            System.out.println("사칙연산 기호가 아닙니다.");
+        }
+        return result;
     }
 }
--- src/main/java/CalculateMain.java
+++ src/main/java/CalculateMain.java
@@ -7,7 +7,9 @@ public class CalculateMain {
         //1.사용자 문자열 입력
         Scanner scanner = new Scanner(System.in);
         System.out.println("계산할 값과 사칙연산 기호를 입력하세요. 예) 2 + 3 * 5");
+
         String value = scanner.nextLine();
+        System.out.println("입력 값 : " + value);
 
         //2.문자열 분리
         StringUtil util = new StringUtil();
@@ -16,11 +18,19 @@ public class CalculateMain {
 
         //3.입력한 문자열을 숫자와 기호로 출력
         int first = Integer.parseInt(values[0]);
+        System.out.println("first : " + first);
 
-        for(int i = 1; i < values.length; i += 2){
+        //4. 사칙
+        int result = first;
+        for(int i = 1; i < values.length; i += 2) {
             String symbol = values[i];
+            System.out.println("symbol : " + symbol);
 
-            int second = Integer.parseInt(values[i+1]);
+            int second = Integer.parseInt(values[i + 1]);
+            System.out.println("second : " + second);
+
+            result = Calculate.calculate(result, symbol, second);
         }
+        System.out.println("result : " + result);
     }
 }
